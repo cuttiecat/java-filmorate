dff77d055c99071b1a117c7d36bf1de7e57257a4
cuttiecat <godrdinasvetlana@yandex.ru>
2023-06-24 15:06:36 +0400

    10 Cпринт. 2 Попытка.

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -6,8 +6,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
